1560d206d46dbee6698fca5d8ad992b245b2d161
Nalin Kamboj <nalin.1997@gmail.com>
2021-06-15 17:52:38 +0100

    Basic comms with VSC Git API established

--- src/extension.ts
+++ src/extension.ts
@@ -4,9 +4,16 @@ import * as vscode from 'vscode';
 import { ActionsPanel } from './ActionsPanel';
 import { SidebarProvider } from './SidebarProvider';
 
+// Testing
+import { API as GitAPI, GitExtension } from './typings/git'; 
+
 // this method is called when your extension is activated
 // your extension is activated the very first time the command is executed
 export function activate(context: vscode.ExtensionContext) {
+
+	const _root = context.extensionPath;
+
+	console.log("Extension Activated. Path - " + _root);
 
 	const sidebarProvider = new SidebarProvider(context.extensionUri);
 	context.subscriptions.push(
@@ -44,7 +51,44 @@ export function activate(context: vscode.ExtensionContext) {
 			}
 		})
 	);
-}
+
+	// GitHub Token Set
+	context.subscriptions.push(
+		vscode.commands.registerCommand("vsactions.setToken", async () => {
+			const token = await vscode.window.showInputBox({
+				prompt: "Please enter the GitHub Token",
+				title: "GitHub Token",
+			});
+
+			switch (token) {
+				case undefined:
+					vscode.window.showInformationMessage("Token set cancelled");
+					break;
+				case null:
+					vscode.window.showInformationMessage("Please enter a token");
+					break;
+				case "":
+					vscode.window.showInformationMessage("Empty bitch");
+				default:
+					// TODO Use regex match for token validity
+					const prevToken = await context.workspaceState.get("ghToken");
+					prevToken === undefined ? vscode.window.showInformationMessage("Old token not defined") : vscode.window.showInformationMessage("Old Token:" + prevToken);
+					vscode.window.showInformationMessage("Not Empty bitch");
+					await context.workspaceState.update("ghToken", token);
+			}
+		})
+	);
+	
+	const gitExtension = vscode.extensions.getExtension<GitExtension>('vscode.git');
+	var api;
+	if (gitExtension !== undefined) {
+		api = gitExtension.exports.getAPI(1);
+	}
+	if (api) {
+		console.log("Git State: " + api.state);
+		console.log("Git Repos:", api.repositories);
+	}
+}	
 
 // this method is called when your extension is deactivated
 export function deactivate() { }
